ef9a0332bb14dff2b4c9a427473430374692bd5e
RetroJet <rretrojet@gmail.com>
2026-03-30 16:52:20 +0200

    Initial Commit

---
+++ NatifeTask3/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  NatifeTask3
+//
+//  Created by Nazar on 30.03.2026.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ NatifeTask3/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  NatifeTask3
+//
+//  Created by Nazar on 30.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
